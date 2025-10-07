863facc0bc83bbcc0a84a399a2878563895e9c4d
Torben Schinke <torben.schinke@worldiety.de>
2025-10-07 09:45:26 +0200

    fix: wrong license header

--- pointer.go
+++ pointer.go
@@ -1,10 +1,3 @@
-// Copyright (c) 2025 worldiety GmbH
-//
-// This file is part of the NAGO Low-Code Platform.
-// Licensed under the terms specified in the LICENSE file.
-//
-// SPDX-License-Identifier: Custom-License
-
 package option
 
 import (
